6911f7c42af9b6d52b5eb5983173b8d5f6baa373
RokRask <rokas.raskevic@gmail.com>
2024-11-26 20:25:11 +0200

    js done

--- app.js
+++ app.js
@@ -28,26 +28,52 @@ fetch('https://in3.dev/inv/')
         const ulNuolaida = document.getElementById('nuolaidaList');
         const ulViso = document.getElementById('visoList');
 
+        let tarpineSuma = 0;
+
         data.items.forEach(item => {
-            const li = document.createElement('li');
-            li.innerText = item.description;
-            ulPrekes.appendChild(li);
-            li.innerText = item.quantity;
-            ulKiekis.appendChild(li);
-            li.innerText = item.price;
-            ulKaina.appendChild(li);
-            if (item.discount = []) {
-                li.innerText = '-';
+            
+            const liPrekes = document.createElement('li');
+            liPrekes.innerText = item.description;
+            ulPrekes.appendChild(liPrekes);
+
+            const liKiekis = document.createElement('li');
+            liKiekis.innerText = item.quantity;
+            ulKiekis.appendChild(liKiekis);
+
+            const liKaina = document.createElement('li');
+            liKaina.innerText = item.price;
+            ulKaina.appendChild(liKaina);
+
+            const liNuolaida = document.createElement('li');
+            if (item.discount.length === 0) {
+                liNuolaida.innerText = '-';
+            } else if (item.discount.type === 'fixed') {
+                liNuolaida.innerText = `-${item.discount.value}€ (fiksuota)`;
             } else {
-                li.innerText = item.discount.value;
+                const nuolaidaEurais = (item.price * item.quantity) * (item.discount.value / 100);
+                liNuolaida.innerText = `-${item.discount.value}% (${nuolaidaEurais.toFixed(2)}€)`;
             }
-            ulNuolaida.appendChild(li);
+            ulNuolaida.appendChild(liNuolaida);
+
+            const liViso = document.createElement('li');
             if (item.discount.type === 'fixed') {
-                // const suNuolaida = item.price * item.quantity - item.discount.value;
-                li.innerText = `-`
+                liViso.innerText = (item.price * item.quantity - item.discount.value).toFixed(2);
+            } else if (item.discount.type === 'percentage') {
+                const suNuolaida = item.price * item.quantity - ((item.price * item.quantity) * item.discount.value / 100);
+                liViso.innerText = suNuolaida.toFixed(2);
             } else {
-                // const suNuolaida = item.price * item.quantity - (item.price * item.quantity) * item.discount.value / 100;
+                liViso.innerText = (item.price * item.quantity).toFixed(2);
             }
+
+            tarpineSuma += parseInt(liViso.textContent);
+            ulViso.appendChild(liViso);
         });
+        
+        document.getElementById('transportoIslaidos').innerText = `${data.shippingPrice.toFixed(2)}€`;
+        document.getElementById('tarpineSuma').innerText = `${tarpineSuma.toFixed(2)}€`;
+        const pvm = tarpineSuma * 0.21;
+        document.getElementById('pvm').innerText = `${pvm.toFixed(2)}€`;
+        const galutineSuma = tarpineSuma + pvm + data.shippingPrice;
+        document.getElementById('pvm').innerText = `${galutineSuma.toFixed(2)}€`;
     })
     .catch(error => console.error("Klaida gaunant duomenis iš API:", error));
