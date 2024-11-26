3e3f622fa6558541c00a2d8d445f58bdde1e3dd1
RokRask <rokas.raskevic@gmail.com>
2024-11-26 18:52:53 +0200

    html done

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ fetch('https://in3.dev/inv/')
         document.getElementById('saskaitaData').innerText = data.date;
         document.getElementById('apmoketiIki').innerText = data.due_date;
 
-        
+        const pardavejas = document.querySelector('.pardavejas');
+        const pirkejas = document.querySelector('.pirkejas');
+
+        Object.values(data.company.seller).forEach(element => {
+            const p = document.createElement('p');
+            p.innerText = element;
+            pardavejas.appendChild(p);
+        });
     })
     .catch(error => console.error("Klaida gaunant duomenis iš API:", error));
