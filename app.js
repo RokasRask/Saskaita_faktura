fe22f7865a60573601f7a34990a69200817a9c20
RokRask <rokas.raskevic@gmail.com>
2024-11-26 19:37:51 +0200

    html done

--- app.js
+++ app.js
@@ -15,5 +15,39 @@ fetch('https://in3.dev/inv/')
             p.innerText = element;
             pardavejas.appendChild(p);
         });
+
+        Object.values(data.company.buyer).forEach(element => {
+            const p = document.createElement('p');
+            p.innerText = element;
+            pirkejas.appendChild(p);
+        });
+
+        const ulPrekes = document.getElementById('prekesList');
+        const ulKiekis = document.getElementById('kiekisList');
+        const ulKaina = document.getElementById('kainaVntList');
+        const ulNuolaida = document.getElementById('nuolaidaList');
+        const ulViso = document.getElementById('visoList');
+
+        data.items.forEach(item => {
+            const li = document.createElement('li');
+            li.innerText = item.description;
+            ulPrekes.appendChild(li);
+            li.innerText = item.quantity;
+            ulKiekis.appendChild(li);
+            li.innerText = item.price;
+            ulKaina.appendChild(li);
+            if (item.discount = []) {
+                li.innerText = '-';
+            } else {
+                li.innerText = item.discount.value;
+            }
+            ulNuolaida.appendChild(li);
+            if (item.discount.type === 'fixed') {
+                // const suNuolaida = item.price * item.quantity - item.discount.value;
+                li.innerText = `-`
+            } else {
+                // const suNuolaida = item.price * item.quantity - (item.price * item.quantity) * item.discount.value / 100;
+            }
+        });
     })
     .catch(error => console.error("Klaida gaunant duomenis iš API:", error));
